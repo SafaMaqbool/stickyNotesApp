e6f80a291428995599db02a98c45361d9f1bb7ff
Safa Maqbool <s.maq.basha@gmail.com>
2024-07-30 02:54:08 +0500

    static notes created

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import NotesPage from './pages/NotesPage'
+
+const App = () => {
+  return (
+    <div id='app'>
+      <NotesPage/>
+    </div>
+  )
+}
+
+export default App
