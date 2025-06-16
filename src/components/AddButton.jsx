3b0c9624eb4d3e3a51b63114867fce116462b3f9
SafaMaqbool <s.maq.basha@gmail.com>
2025-06-16 22:36:39 +0500

    feat: add Controls and AddButton components, and integrate them into NotesPage

---
+++ src/components/AddButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Plus from '../icons/Plus'
+const AddButton = () => {
+  return (
+    <div
+    id='add-btn'> <Plus/></div>
+  )
+}
+
+export default AddButton
--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,13 +1,15 @@
 import NoteCard from "../components/NoteCard";
 import { useContext } from "react";
 import { NoteContext } from "../context/NoteContext";
+import Controls from "../components/Controls";
 const NotesPage = () => {
   const {notes}=useContext(NoteContext);
   return (
     <div>
       {notes.map((note) => (
-        <NoteCard note={note} key={note.$id}  />
+        <NoteCard note={note} key={note.$id} />
       ))}
+      <Controls />
     </div>
   );
 };
