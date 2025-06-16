ec3eca6ae1c20dc778eea3b70dade0ff3a73e49a
SafaMaqbool <s.maq.basha@gmail.com>
2025-06-16 23:16:23 +0500

    refactor: clean up AddButton component structure and improve readability

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import Plus from '../icons/Plus'
-import { useRef } from 'react';
-import colors from '../assets/colors.json'
-import { db } from '../appwrite/databases';
+import Plus from "../icons/Plus";
+import { useRef } from "react";
+import colors from "../assets/colors.json";
+import { db } from "../appwrite/databases";
+import { useContext } from "react";
+import { NoteContext } from "../context/NoteContext";
 const AddButton = () => {
+  const { setNotes } = useContext(NoteContext);
+  const startingPos = useRef(10);
 
-    const startingPos = useRef(10);
-
-    const addNote = async () => {
-      const payload = {
-        position: JSON.stringify({
-          x: startingPos.current,
-          y: startingPos.current,
-        }),
-        color: JSON.stringify(colors[0]),
-      };
-startingPos.current += 10;
-      const response = await db.notes.create(payload);
-      console.log("Note added:", response);
+  const addNote = async () => {
+    const payload = {
+      position: JSON.stringify({
+        x: startingPos.current,
+        y: startingPos.current,
+      }),
+      color: JSON.stringify(colors[0]),
     };
- 
-  return (
-    <div
-    id='add-btn'onClick={addNote}> <Plus/></div>
-  )
-}
+    startingPos.current += 10;
+    const response = await db.notes.create(payload);
+    console.log("Note added:", response);
+    setNotes((prevState) => [response, ...prevState]);
+  };
 
-export default AddButton
+  return (
+    <div id="add-btn" onClick={addNote}>
+      {" "}
+      <Plus />
+    </div>
+  );
+};
+
+export default AddButton;
