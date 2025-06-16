0fdeef105ee4ce6683d09cdc49a4b358c5ec8685
SafaMaqbool <s.maq.basha@gmail.com>
2025-06-16 23:13:21 +0500

    feat: implement AddButton component for adding notes and include color options

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import Plus from '../icons/Plus'
+import { useRef } from 'react';
+import colors from '../assets/colors.json'
+import { db } from '../appwrite/databases';
 const AddButton = () => {
+
+    const startingPos = useRef(10);
+
+    const addNote = async () => {
+      const payload = {
+        position: JSON.stringify({
+          x: startingPos.current,
+          y: startingPos.current,
+        }),
+        color: JSON.stringify(colors[0]),
+      };
+startingPos.current += 10;
+      const response = await db.notes.create(payload);
+      console.log("Note added:", response);
+    };
+ 
   return (
     <div
-    id='add-btn'> <Plus/></div>
+    id='add-btn'onClick={addNote}> <Plus/></div>
   )
 }
 
