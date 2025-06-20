6d5014eaca26c85133e97f2b4f86bc9bb4e1dbeb
SafaMaqbool <s.maq.basha@gmail.com>
2025-06-20 23:19:08 +0500

    feat: enhance NotesPage to handle click outside and reset selected note

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -2,10 +2,29 @@ import NoteCard from "../components/NoteCard";
 import { useContext } from "react";
 import { NoteContext } from "../context/NoteContext";
 import Controls from "../components/Controls";
+import { useRef } from "react";
+import { useEffect } from "react";
+
 const NotesPage = () => {
-  const {notes}=useContext(NoteContext);
+
+  const {notes,selectedNote,setSelectedNote}=useContext(NoteContext);
+  const pageRef=useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e) => {
+      const isNote = e.target.closest(".card");
+      const isControls = e.target.closest("#controls");
+
+      if (!isNote && !isControls && selectedNote) {
+        setSelectedNote(null);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => document.removeEventListener("mousedown", handleClickOutside);
+  }, [selectedNote]);
   return (
-    <div>
+    <div ref={pageRef}>
       {notes.map((note) => (
         <NoteCard note={note} key={note.$id} />
       ))}
